1dd0434eb5e838e2e741072ab14f9b24d4a182cb
Anurag Kulkarni <kanurag0801@gmail.com>
2021-05-23 16:17:04 +0000

    Backend Complete

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -8,7 +8,13 @@ router.get('/', async(req,res) => {
     .catch((err) => res.status(400).json("Error: "+err));
 })
 
-router.post('/', async(req,res) => {
+router.get('/:id', async(req,res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json("Error: "+err));
+})
+
+router.post('/add', async(req,res) => {
   
   const newExercise = new Exercise({
     username: req.body.username,
@@ -21,6 +27,29 @@ router.post('/', async(req,res) => {
     .then(() => res.json("Exercise Added!"))
     .catch((err) => res.status(400).json("Error: "+err));
   
+})
+
+router.post('/update/:id', async(req,res) => {
+  
+  Exercise.findById(req.params.id)
+    .then((exercise) => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+      
+      exercise.save()
+        .then(() => res.json("Exercise Updated!"))
+        .catch((err) => res.status(400).json("Error: "+err));
+    })
+    .catch((err) => res.status(400).json("Error: "+err));
+  
+})
+
+router.delete('/:id', async(req,res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise Deleted!"))
+    .catch((err) => res.status(400).json("Error: "+err));
 })
 
 module.exports = router;
