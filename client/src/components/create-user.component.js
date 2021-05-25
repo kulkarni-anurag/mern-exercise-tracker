e1a35c33f5b7cca0dae70b9927b5527edb3fe74b
Anurag Kulkarni <kanurag0801@gmail.com>
2021-05-25 13:36:05 +0000

    Frontend Complete

--- client/src/components/create-user.component.js
+++ client/src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
   constructor(props){
@@ -25,7 +26,9 @@ export default class CreateUser extends Component {
       username: this.state.username
     }
     
-    console.log(user);
+    axios.post('http://localhost:5000/users/add', user)
+      .then((response) => console.log(response.data))
+      .catch((err) => console.log("Error: "+err));
     
     this.setState({
       username: ''
